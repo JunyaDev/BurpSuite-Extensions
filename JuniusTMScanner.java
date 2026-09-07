475e28d2c86c2e3f8bcf45417ebcdc3bc0c76a74
JunyaDev <JunyaDev@proton.me>
2026-09-07 21:45:14 +0300

    Fixing up the messed up Regex pt.2

--- JuniusTMScanner.java
+++ JuniusTMScanner.java
@@ -55,16 +55,16 @@ class Form {
   public void computePatterns() {
     formPattern = Pattern.compile("<form\\b[^>]>[\\s\\S]?<\/form>", Pattern.CASE_INSENSITIVE); 
 
-    idPattern = Pattern.compile("<form\\b[^>]\\bid\\s=\\s"([\\w])\"", Pattern.CASE_INSENSITIVE);
-    namePattern = Pattern.compile("<form\\b[^>]\\bname\\s=\\s"([\\w])\"", Pattern.CASE_INSENSITIVE);
-    actionPattern = Pattern.compile("<form\\b[^>]\\baction\\s=\\s"([:\\/\\-\\w.])\"", Pattern.CASE_INSENSITIVE); 
-    methodPattern = Pattern.compile("<form\\b[^>]\\bmethod\\s=\\s"([\\w])\"", Pattern.CASE_INSENSITIVE); 
+    idPattern = Pattern.compile("<form\\b[^>]\\bid\\s=\\s\"([\\w])\"", Pattern.CASE_INSENSITIVE);
+    namePattern = Pattern.compile("<form\\b[^>]\\bname\\s=\\s\"([\\w])\"", Pattern.CASE_INSENSITIVE);
+    actionPattern = Pattern.compile("<form\\b[^>]\\baction\\s=\\s\"([:\\/\\-\\w.])\"", Pattern.CASE_INSENSITIVE); 
+    methodPattern = Pattern.compile("<form\\b[^>]\\bmethod\\s=\\s\"([\\w])\"", Pattern.CASE_INSENSITIVE); 
     inputPattern = Pattern.compile("<input\\b[^>]>", Pattern.CASE_INSENSITIVE); 
 
-    inputTypePattern = Pattern.compile("<input\\b[^>]\\btype\\s=\\s"([\\w])\"", Pattern.CASE_INSENSITIVE); 
-    inputNamePattern = Pattern.compile("<input\\b[^>]\\bname\\s=\\s"([\\w])\"", Pattern.CASE_INSENSITIVE); 
-    inputIdPattern = Pattern.compile("<input\\b[^>]\\bid\\s=\\s"([\\w])\"", Pattern.CASE_INSENSITIVE); 
-    inputValuePattern = Pattern.compile("<input\\b[^>]\\bvalue\\s=\\s"([\\w]*)\"", Pattern.CASE_INSENSITIVE);
+    inputTypePattern = Pattern.compile("<input\\b[^>]\\btype\\s=\\s\"([\\w])\"", Pattern.CASE_INSENSITIVE); 
+    inputNamePattern = Pattern.compile("<input\\b[^>]\\bname\\s=\\s\"([\\w])\"", Pattern.CASE_INSENSITIVE); 
+    inputIdPattern = Pattern.compile("<input\\b[^>]\\bid\\s=\\s\"([\\w])\"", Pattern.CASE_INSENSITIVE); 
+    inputValuePattern = Pattern.compile("<input\\b[^>]\\bvalue\\s=\\s\"([\\w]*)\"", Pattern.CASE_INSENSITIVE);
   }
 
   public HttpRequest getHttpRequest() {
